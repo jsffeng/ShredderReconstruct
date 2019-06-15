e2147d1da7891779c68b2d3e5a9b8e4685e6aa18
jsffeng <jsf_feng@sina.com>
2019-06-15 20:22:01 +1200

    Removing an extra comma in several places in the comment and Doc

--- text_unshredder.cpp
+++ text_unshredder.cpp
@@ -9,7 +9,7 @@
 //  c1|c2|c3
 //  d1|d2|d3
 // 
-//  In above example,, a1, a2, b1, etc. is a string with same width, "|" is the delimiter
+//  In above example, a1, a2 or b1, etc. is a string with same width, "|" is the delimiter
 //  used by programme.
 //
 //  In the programme, above data will be stored into following 2 dimensional vector:
